f3ce6c785bdebe0b29424e9bf011d05fb5fae9b0
Logan Wu <yik2@pdx.edu>
2015-11-24 12:27:44 -0800

    update app.js and lib

---
+++ app.js
@@ -0,0 +1,16 @@
+var Todos = angular.module('Todos', ['ui.router']);
+
+Todos.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('todos', {
+    url: "/todos",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+      },
+      'body': {
+        templateUrl: "partials/todos.html",
+        controller: "TodosCtrl"
+      },
+    }
+});
+});
