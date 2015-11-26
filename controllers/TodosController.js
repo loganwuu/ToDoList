1d240f2d8821a8a21db5f6dd1af9b87016c52a15
Logan Wu <yik2@pdx.edu>
2015-11-26 11:16:43 -0800

    delete all files

--- controllers/TodosController.js
+++
@@ -1,7 +0,0 @@
-Todos.controller('TodosCtrl', function TodosCtrl($scope, $stateParams, TodosFactory, UtilitiesFactory) {
-  $scope.todos = TodosFactory.todos;
-  
-  $scope.currentTodo = UtilitiesFactory.findById($scope.todos, $stateParams.todoId);
-
-  $scope.TodosFactory = TodosFactory;
-});
