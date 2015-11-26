276a8bcd02025d6287eaa8f97c759d82fd860913
Logan Wu <yik2@pdx.edu>
2015-11-26 12:49:59 -0800

    add main.js

---
+++ scripts/controllers/main.js
@@ -0,0 +1,35 @@
+main.js
+'use strict';
+
+angular.module('todoListApp')
+.controller('mainCtrl', function($scope, TodosFactory, dataService) {
+  $scope.todos = TodosFactory.todos;
+  
+  $scope.TodosFactory = TodosFactory;
+  
+//  $scope.addTodo = function() {
+//    $scope.todos.unshift({name: $scope.name});
+//    $scope.name = null;
+//  };
+
+//  $scope.helloConsole = dataService.helloConsole;
+
+  dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+  
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+  
+  $scope.saveTodos = function() {
+    var filteredTodos = $scope.todos.filter(function(todo) {
+      if(todo.edited) {
+        return todo;
+      };
+    });
+    dataService.saveTodos(filteredTodos);
+  };
+})
